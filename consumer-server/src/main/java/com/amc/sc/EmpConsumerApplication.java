772836a5783711de1f115b2e554d1efcf17c13b8
zwb <1822828389@qq.com>
2021-01-03 14:13:13 +0800

    调整配置文件位置

--- consumer-server/src/main/java/com/amc/sc/EmpConsumerApplication.java
+++ consumer-server/src/main/java/com/amc/sc/EmpConsumerApplication.java
@@ -2,17 +2,10 @@ package com.amc.sc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class EmpConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmpConsumerApplication.class);
-    }
-
-    @Bean
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
     }
 }
